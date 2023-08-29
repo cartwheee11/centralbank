6cf657ca2126644bc3b92c7e7dd41b65368e133d
vane4ka <vane4ka@MBP-vane4ka.Dlink>
2023-08-29 19:00:50 +0300

    добавлена возможность авторизации

---
+++ api/login.js
@@ -0,0 +1,38 @@
+
+
+const db = require('../src/service/getDb.js');
+
+const fauna = require('faunadb');
+const q = fauna.query;
+
+export default async function (req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  const { nickname, pass } = JSON.parse(req.body);
+
+  if(nickname && pass) {
+    const response = await db.query(
+      q.Map(
+        q.Paginate(q.Match(q.Index('user_by_nickname'), nickname)),
+        q.Lambda('elem', q.Get(q.Var('elem')))
+      )
+    )
+
+    if(response.data[0]) {
+      const user = response.data[0].data;
+      if (pass === user.pass) {
+        const { nickname, birthDate, registration, role, email, deposit } = user;
+
+        res.json({ success: true, user: {
+          nickname, birthDate, registration, role, email, deposit
+        } })
+      } else {
+        res.json({ success: false, message: "Вы ввели неверный пароль" })
+      }
+    } else {
+      res.json({ success: false, message: "Пользователь с таким именем не найден" })
+    }
+  } else {
+    res.json({ success: false, message: "Поля не должны быть пустыми" })
+  }
+}
--- public/js/api.js
+++ public/js/api.js
@@ -7,3 +7,15 @@ export async function submit(name, email, reason) {
   const json = res.json();
   return json;
 }
+
+export async function login(nickname, pass) {
+  const res = await fetch('/api/login', {
+    method: 'post',
+    body: JSON.stringify({nickname, pass})
+  });
+
+  console.log(res)
+
+  const json = res.json();
+  return json;
+}
