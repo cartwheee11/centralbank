9591d0e2b863a94843e87241b1024ae7acfdc335
vane4ka <vane4ka@MBP-vane4ka.Dlink>
2023-09-01 14:44:05 +0300

    добалена кнопка перехода в контрольную панель и обратно

--- public/js/checkauth.js
+++ public/js/checkauth.js
@@ -4,11 +4,23 @@ const auth = JSON.parse(localStorage.getItem('auth'));
 if(auth) {
   api.login(auth.nickname, auth.pass).then(res => {
     if(!res.success) {
+      console.log(res.user.role)
       localStorage.removeItem('auth');
       localStorage.removeItem('user');
 
       location.reload();
+    } else {
+      if(res.user.role == 'admin') {
+        const el = document.createElement('a');
+        el.href = '/cp.html';
+        console.log(res.user.role)
+        el.style = 'position: fixed; bottom: 10px; right: 10px; background-color: #f5cb7c; padding: 10px 20px; color: #6a4808; border-radius: 100px; box-shadow: 0px 7px 21px 0 rgba(34, 60, 80, 0.2);';
+        el.innerText = 'Контрольная панель';
+        document.body.append(el);
+      }
     }
   })  
 }
+
+
 
