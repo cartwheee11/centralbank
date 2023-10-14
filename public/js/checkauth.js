04ba814d4b443dea4fb6e5eae85285498e37b9ab
cor4min <coramin81375698127@gmail.com>
2023-10-14 16:00:17 +0200

    Update checkauth.js

--- public/js/checkauth.js
+++ public/js/checkauth.js
@@ -10,12 +10,12 @@ if(auth) {
       location.reload();
     } else {
       localStorage.setItem('user', JSON.stringify(res.user))
-      if(res.user.role == 'admin') {
+      if(res.user.role == 'Менеджер') {
 
         const el = document.createElement('a');
         el.href = '/cp.html';
         el.style = 'position: fixed; bottom: 10px; right: 10px; background-color: #f5cb7c; padding: 10px 20px; color: #6a4808; border-radius: 100px; box-shadow: 0px 7px 21px 0 rgba(34, 60, 80, 0.2);';
-        el.innerText = 'Контрольная панель';
+        el.innerText = 'Панель управления EEb Pro';
         document.body.append(el);
       }
     }
